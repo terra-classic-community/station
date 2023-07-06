ea42fd8306b889cd7d88695c2193ddde27b476b3
echel0n <echel0n@terrarebels.net>
2023-07-05 21:28:51 -0700

    fix: desktop pwa app to use web extension

--- src/auth/scripts/env.ts
+++ src/auth/scripts/env.ts
@@ -1,7 +1,10 @@
 import { debug } from "utils/env"
 
+const isMobile = (): boolean => {
+  return (
+    window.matchMedia("(display-mode: standalone)").matches &&
+    navigator.userAgent.indexOf("Mobi") > -1
+  )
+}
 export const sandbox =
-  debug.auth ||
-  process.env.REACT_APP_SANDBOX === "true" ||
-  navigator.userAgent.includes("Electron") ||
-  window.matchMedia("(display-mode: standalone)").matches
+  debug.auth || process.env.REACT_APP_SANDBOX === "true" || isMobile()
