2302fd021af4eefb353819bf4076ca1444f70dc2
echel0n <echel0n@terrarebels.net>
2023-07-05 18:23:31 -0700

    fix: desktop electron compatibility

--- src/auth/scripts/env.ts
+++ src/auth/scripts/env.ts
@@ -3,4 +3,5 @@ import { debug } from "utils/env"
 export const sandbox =
   debug.auth ||
   process.env.REACT_APP_SANDBOX === "true" ||
+  navigator.userAgent.includes("Electron") ||
   window.matchMedia("(display-mode: standalone)").matches
